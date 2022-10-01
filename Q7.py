dc8207a0c07a6d7bdb145101b09b7b0dcdf7d031
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-10-01 19:53:30 +0530

    Add files via upload

--- Q7.py
+++ Q7.py
@@ -1,5 +1,8 @@
 #Write a Python script to remove all non int values from a list.
-l=["kajal",9,"puja",4]
-l.remove(9)
-l.remove(4)
-print(l)
+l=["kajal",9,"puja",4,8,"deepak"]
+l2=[]
+for a in l:
+    if type(a)==int:
+      l2.append(a)
+
+print(l2)
